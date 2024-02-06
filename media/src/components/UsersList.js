7e84d18b59a0adb2fcdeaedd02b9a987a2a66e87
Ranita Saha <saharanitaa1230@gmail.com>
2024-02-06 23:05:03 +0530

    feat: added custom hook

--- media/src/components/UsersList.js
+++ media/src/components/UsersList.js
@@ -1,42 +1,48 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { addUser, fetchUsers } from "../store";
 import Button from "./Button";
 import Skeleton from "./Skeleton";
+import { useThunk } from "../hooks/use-thunk";
 
 const UsersList = () => {
-  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
-  const [loadingUsersError, setLoadingUsersError] = useState(null);
+  // const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+  // const [loadingUsersError, setLoadingUsersError] = useState(null);
 
-  const [isCreatingUser, setIsCreatingUser] = useState(false);
-  const [creatingUsersError, setCreatingUserError] = useState(null);
+  const [doFetchUsers, isLoadingUsers, loadingUsersError] =
+    useThunk(fetchUsers);
+  const [doAddUsers, isCreatingUser, creatingUsersError] = useThunk(addUser);
 
-  const dispatch = useDispatch();
+  // const [isCreatingUser, setIsCreatingUser] = useState(false);
+  // const [creatingUsersError, setCreatingUserError] = useState(null);
+
   const { data } = useSelector((state) => state.users);
 
   useEffect(() => {
-    setIsLoadingUsers(true);
-    dispatch(fetchUsers())
-      .unwrap() //bcoz dispatch calls .then even if the promise fails, so wrap will give a brand new promise anf if the request is successful then .then else .catch
-      // .then(()=>{
-      //   console.log("suceess");
-      //   // setIsLoadingUsers(false);
-      // })
-      .catch((err) => {
-        console.log("error");
-        setLoadingUsersError(err);
-        // setIsLoadingUsers(false);
-      })
-      //after all then and catch finally will be called
-      .finally(() => setIsLoadingUsers(false));
+    // setIsLoadingUsers(true);
+    // dispatch(fetchUsers())
+    //   .unwrap() //bcoz dispatch calls .then even if the promise fails, so wrap will give a brand new promise anf if the request is successful then .then else .catch
+    //   // .then(()=>{
+    //   //   console.log("suceess");
+    //   //   // setIsLoadingUsers(false);
+    //   // })
+    //   .catch((err) => {
+    //     console.log("error");
+    //     setLoadingUsersError(err);
+    //     // setIsLoadingUsers(false);
+    //   })
+    //   //after all then and catch finally will be called
+    //   .finally(() => setIsLoadingUsers(false));
+    doFetchUsers();
   }, []);
 
   const handleUserAdd = () => {
-    setIsCreatingUser(true);
-    dispatch(addUser())
-      .unwrap()
-      .catch((err) => setCreatingUserError(err))
-      .finally(() => setIsCreatingUser(false));
+    // setIsCreatingUser(true);
+    // dispatch(addUser())
+    //   .unwrap()
+    //   .catch((err) => setCreatingUserError(err))
+    //   .finally(() => setIsCreatingUser(false));
+    doAddUsers();
   };
 
   if (isLoadingUsers) {
@@ -59,8 +65,11 @@ const UsersList = () => {
     <div>
       <div className="flex flex-row justify-between m-3">
         <h1 className="m-2 text-xl">Users</h1>
-        {isCreatingUser ? "Creaing User..." :
-        <Button onClick={handleUserAdd}>+ Add User</Button>}
+        {isCreatingUser ? (
+          "Creaing User..."
+        ) : (
+          <Button onClick={handleUserAdd}>+ Add User</Button>
+        )}
         {creatingUsersError && "Error Creating User..."}
       </div>
       {renderedUsers}
