102d50bd2fdd5086eb7af0a61fee1c251700d2bc
Ranita Saha <saharanitaa1230@gmail.com>
2024-02-06 22:43:27 +0530

    feat:fine grained loading state

--- media/src/components/UsersList.js
+++ media/src/components/UsersList.js
@@ -1,26 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, fetchUsers } from "../store";
 import Button from "./Button";
 import Skeleton from "./Skeleton";
 
 const UsersList = () => {
+  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+  const [loadingUsersError, setLoadingUsersError] = useState(null);
+
+  const [isCreatingUser, setIsCreatingUser] = useState(false);
+  const [creatingUsersError, setCreatingUserError] = useState(null);
+
   const dispatch = useDispatch();
-  const { data, isLoading, error } = useSelector((state) => state.users);
+  const { data } = useSelector((state) => state.users);
 
   useEffect(() => {
-    dispatch(fetchUsers());
+    setIsLoadingUsers(true);
+    dispatch(fetchUsers())
+      .unwrap() //bcoz dispatch calls .then even if the promise fails, so wrap will give a brand new promise anf if the request is successful then .then else .catch
+      // .then(()=>{
+      //   console.log("suceess");
+      //   // setIsLoadingUsers(false);
+      // })
+      .catch((err) => {
+        console.log("error");
+        setLoadingUsersError(err);
+        // setIsLoadingUsers(false);
+      })
+      //after all then and catch finally will be called
+      .finally(() => setIsLoadingUsers(false));
   }, []);
 
   const handleUserAdd = () => {
-    dispatch(addUser());
+    setIsCreatingUser(true);
+    dispatch(addUser())
+      .unwrap()
+      .catch((err) => setCreatingUserError(err))
+      .finally(() => setIsCreatingUser(false));
   };
 
-  if (isLoading) {
+  if (isLoadingUsers) {
     return <Skeleton times={6} className="h-10 w-full" />;
   }
 
-  if (error) return <div>Error fetching data...</div>;
+  if (loadingUsersError) return <div>Error fetching data...</div>;
 
   const renderedUsers = data.map((user) => {
     return (
@@ -36,7 +59,9 @@ const UsersList = () => {
     <div>
       <div className="flex flex-row justify-between m-3">
         <h1 className="m-2 text-xl">Users</h1>
-        <Button onClick={handleUserAdd}>+ Add User</Button>
+        {isCreatingUser ? "Creaing User..." :
+        <Button onClick={handleUserAdd}>+ Add User</Button>}
+        {creatingUsersError && "Error Creating User..."}
       </div>
       {renderedUsers}
     </div>
