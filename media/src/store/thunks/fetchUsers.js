f4b3b9bcda373d220519cfb979f8575fb41081a7
Ranita Saha <saharanitaa1230@gmail.com>
2024-02-09 22:33:10 +0530

    feat: refactor code

--- media/src/store/thunks/fetchUsers.js
+++ media/src/store/thunks/fetchUsers.js
@@ -2,14 +2,14 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
-  const response = await axios.get("http://localhost:3005/users");
+  const response = await axios.get("ttp://localhost:3005/users");
 
   return response.data;
 });
 
-automatically generated:
-fetchUsers.pending==='users/fetch/pending'
-fetchUsers.fulfilled==='users/fetch/fulfilled'
-fetchUsers.rejected==='users/fetch/rejected'
+//automatically generated:
+// fetchUsers.pending==='users/fetch/pending'
+// fetchUsers.fulfilled==='users/fetch/fulfilled'
+// fetchUsers.rejected==='users/fetch/rejected'
 
 export { fetchUsers };
