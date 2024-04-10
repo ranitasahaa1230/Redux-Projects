bf50ac47714baa9afcb95ea6c56861d0e179e436
Ranita Saha <saharanitaa1230@gmail.com>
2024-04-10 18:40:31 +0530

    feat:rtk query

--- media/src/components/AlbumsList.js
+++ media/src/components/AlbumsList.js
@@ -1,5 +1,43 @@
+import { useFetchAlbumsQuery,useAddAlbumMutation } from "../store";
+import Button from "./Button";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+
 function AlbumsList({ user }) {
-  return <div>Albums for {user.name}</div>;
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const [addAlbum, results]=useAddAlbumMutation();
+
+  const handleAddAlbum=()=>{
+    addAlbum(user)
+  };
+
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={3} />;
+  } else if (error) {
+    content = <div>Error loading albums...</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of photis in the album
+        </ExpandablePanel>
+      );
+    });
+  }
+
+  console.log(data, error, isLoading);
+
+  return (
+    <div>
+      <div>Albums for {user.name}
+      <Button onClick={handleAddAlbum}>+ New Album</Button>
+
+      </div>
+      <div>{content}</div>
+    </div>
+  );
 }
 
 export default AlbumsList;
--- media/src/store/index.js
+++ media/src/store/index.js
@@ -1,14 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from '@reduxjs/toolkit/query'
 import { usersReducer } from "./slices/usersSlices";
+import { albumsApi } from "./apis/albumsApi";
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
+    [albumsApi.reducerPath]: albumsApi.reducer, //'albums'=>the key should be same
   },
+  middleware:(getDefaultMiddleware)=>{
+    return getDefaultMiddleware().concat(albumsApi.middleware);
+  }
+
 });
+
+setupListeners(store.dispatch);
 
 export * from "./thunks/fetchUsers";
 export * from "./thunks/addUser";
 export * from "./thunks/deleteUser";
-
-
+export {useFetchAlbumsQuery, useAddAlbumMutation} from './apis/albumsApi';
